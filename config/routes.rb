1909ae37c097d6fb2dc1233ccffca5d76e8cee67
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-22 21:00:58 +0900

    [add] マイページルートとsongsモデル関連のルートを追加
    mypage#indexへのルート追加と、resources :songs により曲の一覧・登録・編集・削除機能のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
+  # ユーザー登録／ログイン関連
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
@@ -13,4 +14,10 @@ Rails.application.routes.draw do
   get 'users/profile', to: 'profiles#show', as: 'user_profile'
   get 'users/profile/edit', to: 'profiles#edit', as: 'edit_user_profile'
   patch 'users/profile', to: 'profiles#update'
+
+  # マイページ
+  get 'mypage', to: 'mypage#index'
+
+  # Songモデル関連
+  resources :songs
 end
