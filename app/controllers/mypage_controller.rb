41702ec2db4072d0683a7002d1056de8ae999afe
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-24 20:15:16 +0900

    add: マイページにスコア平均表示用の変数@score_averageを追加
    ・current_userのpracticesからscoreの平均を算出し、小数第1位で丸めて@score_averageに代入
    ・ビューでの平均スコア表示に活用

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -5,5 +5,6 @@ class MypageController < ApplicationController
     @user = current_user
     @songs = current_user.songs
     @practices = current_user.practices
+    @score_average = current_user.practices.average(:score).to_f.round(1)
   end
 end
