4d599a0602d84cf9ce9d290d334afb380674ff44
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-24 18:00:01 +0900

    add: Songモデルにkeyとcommentのバリデーションを追加（keyは整数、commentは200文字以内）

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ApplicationRecord
   belongs_to :user
 
   validates :title, :artist, presence: true
+  validates :key, numericality: { only_integer: true }, allow_nil: true
+  validates :comment, length: { maximum: 200 }, allow_blank: true
 end
