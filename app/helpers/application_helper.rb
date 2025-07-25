2b8f4a76d7345f759ceb22810be722c3f0f307e4
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-25 21:06:41 +0900

    add: 楽曲レパートリーの登録などで使用するジャンル一覧のgenre_optionsヘルパーを追加
    ジャンルの選択肢を共通化するため、ApplicationHelperにgenre_optionsメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def genre_options
+    [
+      "J-POP",
+      "ロック",
+      "バラード",
+      "アニメ・ボカロ",
+      "K-POP/洋楽",
+      "アイドルソング",
+      "ヒップホップ/R&B",
+      "演歌・歌謡曲"
+    ]
+  end
 end
