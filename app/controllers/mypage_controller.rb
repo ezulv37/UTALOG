8c6efedea3bfd4c4d1b67bfc35479da030cdc33d
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-27 12:58:18 +0900

    add: マイページに練習ログ・楽曲レパートリーの検索機能を追加
    ・practice_q で練習ログ（title, artist）を部分一致検索
    ・song_q でレパートリー（title, artist）を部分一致検索
    ・genres によるジャンル絞り込み機能も追加

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -6,5 +6,21 @@ class MypageController < ApplicationController
     @songs = current_user.songs.order(created_at: :desc)
     @practices = current_user.practices.order(created_at: :desc)
     @score_average = current_user.practices.average(:score).to_f.round(1)
+
+    # 練習ログの検索
+    if params[:practice_q].present?
+      keyword = "%#{params[:practice_q]}%"
+      @practices = @practices.where("title LIKE :kw OR artist LIKE :kw", kw: keyword)
+    end
+
+    # 楽曲レパートリーの検索
+    if params[:song_q].present?
+      keyword = "%#{params[:song_q]}%"
+      @songs = @songs.where("title LIKE :kw OR artist LIKE :kw", kw: keyword)
+    end
+
+    if params[:genres].present?
+      @songs = @songs.where(genre: params[:genres])
+    end
   end
 end
