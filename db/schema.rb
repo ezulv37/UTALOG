f2ec598656c9537c8a4c4d143bd4a35fd9b860cf
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-31 22:32:11 +0900

    chore: update schema.rb after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_16_025322) do
+ActiveRecord::Schema.define(version: 2025_07_23_070752) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,20 @@ ActiveRecord::Schema.define(version: 2025_07_16_025322) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "practices", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.string "artist"
+    t.integer "key"
+    t.text "comment"
+    t.integer "score"
+    t.string "result_image1"
+    t.string "result_image2"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_practices_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -68,5 +82,6 @@ ActiveRecord::Schema.define(version: 2025_07_16_025322) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "practices", "users"
   add_foreign_key "songs", "users"
 end
