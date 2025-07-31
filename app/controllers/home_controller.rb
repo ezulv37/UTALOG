3e06b9badd46edaa26ffb8130ce19fd10812c7c4
ezu.lv37 <ezu.lv37@gmail.com>
2025-07-31 18:58:55 +0900

    add: YouTube動画検索機能をトップページに追加
    ・検索ワードが入力されている場合はそのキーワードで動画検索を実行 ・検索ワードが空の場合はデフォルトで「歌唱 テクニック」を検索 ・検索結果は@videosに格納してビューで表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,32 @@
 class HomeController < ApplicationController
   def index
     @practice_logs = Practice.includes(user: { image_attachment: :blob }).order(created_at: :desc)
+
+    youtube = Rails.application.config.youtube_service
+    if params[:query].present?
+      @videos = search_youtube_videos(params[:query], youtube)
+    else
+      @videos = search_youtube_videos("歌唱 テクニック", youtube)
+    end
+  end
+
+  private
+
+  def search_youtube_videos(query, youtube)
+    response = youtube.list_searches(
+      'snippet',
+      q: query,
+      type: 'video',
+      max_results: 1,
+      video_embeddable: 'true'
+    )
+
+    response.items.select { |item| item.id.kind == 'youtube#video' }.map do |item|
+      {
+        title: item.snippet.title,
+        video_id: item.id.video_id,
+        thumbnail_url: item.snippet.thumbnails.default.url
+      }
+    end
   end
 end
