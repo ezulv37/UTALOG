8dd8ed0ad7ee74efa90d94add0afc6828d4def90
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-05 20:15:42 +0900

    refactor:未使用のコメントアウトされたコードを削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
   def new
     super
@@ -17,11 +15,4 @@ class Users::SessionsController < Devise::SessionsController
   def destroy
     super
   end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
