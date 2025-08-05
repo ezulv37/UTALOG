7a7e980832fb7c5d51560725e32b60878131f888
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-05 14:56:51 +0900

    add: Userモデルにnameのバリデーションを追加
    nameカラムにpresence: trueとlength: { maximum: 25 }を追加して入力必須かつ文字数制限を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 25 }
+
   has_one_attached :image
 
   has_many :songs, dependent: :destroy
