c73619fcd0b456051892b99fcc68398e219cd077
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-05 19:33:59 +0900

    style: Floatへの変換を明示的にしてLint警告を修正
    ・to_f を Float() に変更し、RubocopのLint/NumberConversion警告に対応

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -5,7 +5,7 @@ class MypageController < ApplicationController
     @user = current_user
     @songs = current_user.songs.order(created_at: :desc)
     @practices = current_user.practices.order(created_at: :desc)
-    @score_average = current_user.practices.average(:score).to_f.round(1)
+    @score_average = Float(current_user.practices.average(:score)).round(1)
 
     # 練習ログの検索
     if params[:practice_q].present?
