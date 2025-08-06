aa0d26c7d38a077e82e7def9a49fade781f62663
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-06 17:22:08 +0900

    feat: Songモデルにジャンル（genre）のバリデーションを追加
    ・genreにpresence: trueを追加して必須項目に設定

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :user
 
-  validates :title, :artist, presence: true
+  validates :title, :artist, :genre, presence: true
   validates :key, numericality: { only_integer: true }, allow_nil: true
   validates :memo, length: { maximum: 200 }, allow_blank: true
 end
