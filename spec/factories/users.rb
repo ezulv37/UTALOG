219d182fda7bb6ecae2c6fdaed33ea6276958de7
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-09 13:17:06 +0900

    test: Userファクトリに画像添付処理を追加
    user作成後にテスト用画像をActiveStorageで添付

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+
+    after(:create) do |user|
+      user.image.attach(
+        io: Rails.root.join('spec/fixtures/images/test_icon_image.png').open,
+        filename: 'test_icon_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
