b5e69af21adcf4d05c9907bd34dbd000febd449d
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-09 12:50:02 +0900

    refactor: key_displayメソッドをKeyDisplayableモジュールに切り出して適用
    ApplicationRecordからkey_displayメソッドを分離し、KeyDisplayableモジュールに移動
    
    ApplicationRecordにincludeして全モデルで利用可能に

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,15 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def key_display
-    return "原曲" if self.key.nil?
-
-    if self.key == 0
-      "原曲"
-    elsif self.key > 0
-      "原曲+#{self.key}"
-    else
-      "原曲#{self.key}"  # keyが-2なら「原曲-2」
-    end
-  end
+  include KeyDisplayable
 end
