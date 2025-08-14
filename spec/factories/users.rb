dbc12b707ffa3ff34a60d5ff49b35ecefbf67ca6
ezu.lv37 <ezu.lv37@gmail.com>
2025-08-15 00:28:20 +0900

    test: ユーザーファクトリからデフォルト画像の添付を削除
    after(:create)でのActiveStorage画像添付を廃止
    
    必要に応じてテスト内で手動添付する方式に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,13 +4,5 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
-
-    after(:create) do |user|
-      user.image.attach(
-        io: Rails.root.join('spec/fixtures/images/test_icon_image.png').open,
-        filename: 'test_icon_image.png',
-        content_type: 'image/png'
-      )
-    end
   end
 end
