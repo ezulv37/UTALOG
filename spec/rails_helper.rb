7da3592212adb91955383029b00a417f8f441b47
ezu.lv37 <ezu.lv37@gmail.com>
2025-09-04 18:59:03 +0900

    test: transactional fixtures を無効化
    DatabaseCleaner に依存するよう変更し、JS や system テストでの DB トランザクション競合を防ぐ

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,7 +75,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)  # テスト開始前にDBを全クリア
