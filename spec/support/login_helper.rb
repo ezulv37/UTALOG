7f6ede57e28d0c0ef5fec94e5d4a7b0a3f3302bb
ezu.lv37 <ezu.lv37@gmail.com>
2025-09-09 19:03:51 +0900

    test: system specで安定してログインできるようにヘルパーを追加
    rack_test専用のlogin_asではsystem spec(JSあり)でmypage_pathへ遷移せず/users/sign_inへリダイレクトされる問題が発生したため、UI操作でログインするlogin_via_uiメソッドを作成して対応

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -7,4 +7,12 @@ module LoginHelper
     expect(page).to have_current_path(mypage_path)
     expect(page).to have_content(user.name)
   end
+
+  def login_via_ui(user)
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+    expect(page).to have_content(user.name)
+  end
 end
