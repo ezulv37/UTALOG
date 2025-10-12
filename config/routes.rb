00bd1ae8dda9d45f5688c451c4c6e57f7ef76e76
ezu.lv37 <ezu.lv37@gmail.com>
2025-10-12 18:41:59 +0900

    add: ゲストログイン用のルーティングを追加
    users/sessions#guest_log_in にPOSTでアクセスできるようにdevise_scope :userでルートを定義

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
   }
 
+  # ゲストログイン
+  devise_scope :user do
+    post 'users/guest_log_in', to: 'users/sessions#guest_log_in'
+  end
+
   # アカウント詳細ページ
   get 'users/account', to: 'accounts#show', as: 'user_account'
 
