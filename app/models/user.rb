9fb84a4e881a11e8137575dc81d9ded3cc4788e6
ezu.lv37 <ezu.lv37@gmail.com>
2025-10-12 18:39:41 +0900

    add: ゲストユーザー作成メソッドをUserモデルに追加
    ゲストログイン機能のため、Userモデルにself.guestメソッドを定義し、存在しない場合にゲストユーザーを自動生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
 
   has_many :songs, dependent: :destroy
   has_many :practices, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@guest.mail') do |user|
+      user.name = 'ゲスト'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
